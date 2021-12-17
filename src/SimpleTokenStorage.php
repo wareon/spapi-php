d1f1c965110a7a9eadcdb2b4f5cea1eb81f447be
chenzhaohong <1015982458@qq.com>
2021-12-17 21:04:53 +0800

    updateSimpleTokenStorage

--- src/SimpleTokenStorage.php
+++ src/SimpleTokenStorage.php
@@ -35,16 +35,11 @@ class SimpleTokenStorage implements TokenStorageInterface {
         }
         $json[$key] = $value;
         $json = json_encode($json, JSON_UNESCAPED_UNICODE);
-        // 缓存
         $expiresOn = $value['expiresOn'] - time();
         $this->setex($this->cacheKey, $expiresOn, $json);
     }
 
-    /**
-     * 获取存储的值
-     * @param $key
-     * @return mixed
-     */
+
     public function get($key)
     {
         $redis = $this->getNewRedis();
@@ -52,24 +47,13 @@ class SimpleTokenStorage implements TokenStorageInterface {
     }
 
 
-    /**
-     * 如果存储的字符串有时效性，可以用$time来设置时间以秒为单位
-     * @param $key
-     * @param int $time
-     * @param $value
-     * @return mixed
-     */
-    public function setex($key, $time = 0, $value)
+    public function setex($key, $time, $value)
     {
         $redis = $this->getNewRedis();
         return $redis->setex($key, $time, $value);
     }
 
-    /**
-     * @param $prefix
-     * 切换redis配置
-     * @return RedisManager
-     */
+
     protected function getNewRedis($prefix = '')
     {
         if (empty($prefix)) $prefix = $this->prefix;
